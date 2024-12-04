f1d08f26739d661230ae4790817b3858bb4841da
tungnt <kenkz447@gmail.com>
2024-12-04 23:27:07 +0700

    Install Playwright and update requirements for additional dependencies

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 from libs.auth import check_login_status
 
+import os
+os.system("playwright install")
+
 page_login = st.Page("routes/login.py")
 page_agents = st.Page("routes/agents.py")
 
