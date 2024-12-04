1f0155a5ec05a9bd8bf51a30db93bd1876d84a07
tungnt <kenkz447@gmail.com>
2024-12-04 23:03:18 +0700

    Add new profile classes for NhaDatCafeLandVn, NhaDat24h, and Mogi

--- libs/profiles.py
+++ libs/profiles.py
@@ -39,9 +39,73 @@ class BanNha888Profile(ScannerProfile):
         page.click("#mm-0 > div:nth-child(2) > article > section > div.conten > div.pagewrap.page_conten_page > div.left_conten > div > div > div > div > center > div.trangcanhan-info > p:nth-child(2) > a")
         pass
 
+class NhaDatCafeLandVnProfile(ScannerProfile):
+    url = None
+    fields = {
+        "name": "body > div:nth-child(5) > div > div.moigioi-infor > div.moigioi-fullname",
+        "mobile": "#detailTelSpan"
+    }
+
+    def __init__(self, id: int):
+        self.url = f"https://nhadat.cafeland.vn/moi-gioi/khang-tho-dia-0911911021-{id}.html"
+
+    def check_status(self, page):
+        try:
+            page.query_selector(self.fields.get("name")).inner_text()
+        except Exception as e:
+            raise  Exception("No data found")
+        
+    def before_select(self, page):
+        page.click(self.fields.get("mobile"))
+        pass
+
+class NhaDat24hProfile(ScannerProfile):
+    url = None
+    fields = {
+        "name": "#divSearchPage",
+        "mobile": "#content > div > div.ct-in-l > div > div > div:nth-child(1) > center > div.trangcanhan-info > p:nth-child(1) > a"
+    }
+
+    def __init__(self, id: int):
+        self.url = f"https://nhadat24h.net/tv/{id}"
+
+    def check_status(self, page):
+        try:
+            page.query_selector(self.fields.get("name")).inner_text()
+        except Exception as e:
+            raise  Exception("No data found")
+
+    def before_select(self, page):
+        pass
+
+class MogiProfile(ScannerProfile):
+    url = None
+    fields = {
+        "name": "#agent > div.info > h1",
+        "mobile": "#agent > div.info > div.agent-phone > a"
+    }
+
+    def __init__(self, id: int):
+        self.url = f"https://mogi.vn/moi-gioi/0834982539-truong-an-uid{id}"
+
+    def check_status(self, page):
+        try:
+            page.query_selector(self.fields.get("name")).inner_text()
+        except Exception as e:
+            raise  Exception("No data found")
+
+    def before_select(self, page):
+        pass
+
 def select_profile(site:str, id: int):
     if site == "bds.com.vn":
         return BDSProfile(id)
     elif site == "bannha888":
         return BanNha888Profile(id)
+    elif site == "nhadat.cafeland.vn":
+        return NhaDatCafeLandVnProfile(id)
+    elif site == "nhadat24h.net":
+        return NhaDat24hProfile(id)
+    elif site == "mogi.vn":
+        return MogiProfile(id)
     return None
